32f7c89379352d661f67a55b1a289f311f048e85
Lamadrid-Mike <mikealamadrid@hotamil.com>
2022-06-01 19:18:25 -0400

    Sending Email Composer Added

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 import { ScrollView, Text } from "react-native";
 import * as Animatable from "react-native-animatable";
+import * as MailComposer from "expo-mail-composer";
 
 class Contact extends Component {
   constructor(props) {
     super(props);
   }
-
+  //STATIC KEYWORD DEFINES STATIC METHODS FOR CLASSES
   static navigationOptions = {
     title: "Contact Us",
   };
+
+  // MAIL COMPOSER API
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ["campsites@nucamp.co"],
+      subject: "Inquiry",
+      body: "to whom it may concern:",
+    });
+  }
 
   render() {
     return (
@@ -22,6 +32,19 @@ class Contact extends Component {
             <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
             <Text>Phone: 1-206-555-1234</Text>
             <Text>Email: campsites@nucamp.co</Text>
+            <Button
+              title="Send Email"
+              buttonStyle={{ backgroundColor: "#5637DD", margin: 40 }}
+              icon={
+                <Icon
+                  name="envelope-o"
+                  type="font-awesome"
+                  color="#fff"
+                  iconStyle={{ marginRight: 10 }}
+                />
+              }
+              onPress={() => this.sendMail()}
+            />
           </Card>
         </Animatable.View>
       </ScrollView>
--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -11,7 +11,8 @@ import {
 } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
-
+import * as Notifications from "expo-notifications";
+// RESERVATION COMPONENT
 class Reservation extends Component {
   constructor(props) {
     super(props);
@@ -53,6 +54,9 @@ class Reservation extends Component {
         {
           text: "Ok",
           onPress: () => {
+            this.presentLocalNotification(
+              this.state.date.toLocaleDateString("en-US")
+            );
             this.resetForm();
           },
         },
@@ -69,6 +73,32 @@ class Reservation extends Component {
       showCalendar: false,
       showModal: false,
     });
+  }
+  //BAR NOTIFICATION API
+  async presentLocalNotification(date) {
+    function sendNotification() {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+        }),
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Your Campsite Reservation Search",
+          body: `Search for ${date} requested`,
+        },
+        trigger: null,
+      });
+    }
+
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
   }
 
   render() {
